08f987b9ab3030df368f8e23a97a5fdb3001971f
radha-123 <122452886+radha-123@users.noreply.github.com>
2023-08-01 18:56:33 +0530

    Add files via upload

---
+++ ifelse.php
@@ -0,0 +1,13 @@
+<html>
+   <body>
+   <?php
+         $d = date("D");
+          if ($d == "Fri")
+            echo "Have a nice weekend!";
+         elseif ($d == "Wed")
+            echo "Have a nice Wednesday!"; 
+         else
+            echo "Have a nice day!"; 
+      ?>
+      </body>
+</html>
